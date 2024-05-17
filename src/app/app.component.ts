e223e737930400b8bdf4dad3ab076ba524170dc8
andreachirico11 <andreachirico11@gmail.com>
2024-05-17 08:22:53 +0200

    CU-86byu4ec8 first part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { routes } from './app.routes';
 
 @Component({
-  selector: 'app-root',
+  selector: 'mp-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'multi-portfolio';
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    title: 'about',
+    path: 'about',
+    loadComponent: () =>
+      import('./about/about.component').then(({ AboutComponent }) => AboutComponent),
+  },
+  {
+    title: 'contact',
+    path: 'contact',
+    loadComponent: () =>
+      import('./contact/contact.component').then(({ ContactComponent }) => ContactComponent),
+  },
+  {
+    title: 'vimeo',
+    path: 'vimeo',
+    loadComponent: () =>
+      import('./vimeo/vimeo.component').then(({ VimeoComponent }) => VimeoComponent),
+  },
+];
